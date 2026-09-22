2ea78f3375ca56f474a84686ed424c0fa6a3bf47
imlargo <jlargob@unal.edu.co>
2026-09-21 20:27:18 -0500

    feat: add DocsToc component for improved navigation in documentation layout

---
+++ src/lib/components/layout/docs/docs-toc.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Toc from '$lib/components/coral/kit/toc/toc.svelte';
+</script>
+
+<aside class="sticky top-8 hidden h-fit w-48 shrink-0 xl:block">
+	<Toc />
+</aside>
--- src/routes/(docs)/+layout.svelte
+++ src/routes/(docs)/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import DocsSidebar from '$lib/components/layout/docs/docs-sidebar.svelte';
+	import DocsToc from '$lib/components/layout/docs/docs-toc.svelte';
 	import type { LayoutProps } from './$types';
 
 	let { children }: LayoutProps = $props();
@@ -9,8 +10,13 @@
 <Sidebar.Provider style="height: calc(100svh - var(--header-height));">
 	<DocsSidebar />
 	<Sidebar.Inset>
-		<div class="mx-auto w-full max-w-3xl px-8 py-12">
-			{@render children?.()}
+		<div class="flex w-full gap-8 px-8 py-12">
+			<div class="min-w-0 flex-1">
+				<div class="mx-auto max-w-3xl">
+					{@render children?.()}
+				</div>
+			</div>
+			<DocsToc />
 		</div>
 	</Sidebar.Inset>
 </Sidebar.Provider>
